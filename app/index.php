8d6c0ecc1c1d300ffa463a7a69c73cbe4ed61cda
leonardo <leonardogirardileal123@gmail.com>
2026-04-24 13:31:08 -0300

    feat: implementa mini router no index.php para gerenciamento de rotas

--- app/index.php
+++ app/index.php
@@ -1,15 +1,55 @@
 <?php
 
-require_once __DIR__ . '/models/Database.php';
+require_once __DIR__ . '/controllers/AuthController.php';
+require_once __DIR__ . '/controllers/TaskController.php';
 
-try {
-    $pdo = Database::connect();
+// Inicia a sessão 
+session_start();
 
-    echo "✅ Conexão com o banco realizada com sucesso!<br>";
 
-    $stmt = $pdo->query("SELECT NOW()");
-    $time = $stmt->fetch();
+// pega rota
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-} catch (Exception $e) {
-    echo "❌ Erro na conexão: " . $e->getMessage();
+// controllers
+$auth = new AuthController();
+$task = new TaskController();
+
+// Rotas
+switch ($uri) {
+
+    // AUTH
+    case '/':
+    case '/login':
+        $auth->login();
+        break;
+
+    case '/register':
+        $auth->register();
+        break;
+
+    case '/logout':
+        $auth->logout();
+        break;
+
+    // TASKS
+    case '/dashboard':
+        $task->index();
+        break;
+
+    case '/tasks/create':
+        $task->create();
+        break;
+
+    case '/tasks/delete':
+        $task->delete();
+        break;
+
+    case '/tasks/toggle':
+        $task->toggle();
+        break;
+
+    default:
+        http_response_code(404);
+        echo "Página não encontrada";
+        break;
 }
