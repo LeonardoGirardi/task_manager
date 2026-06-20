34961fbc1303e9042669f9c0e2b25b2323bccfb9
leonardo <leonardogirardileal123@gmail.com>
2026-06-20 10:03:08 -0300

    feat(views) Cria pasta baseline para arquivos frontend

---
+++ app/views/baseline/frame_top.php
@@ -0,0 +1,6 @@
+<div class="monitor">
+    <div class="bezel">
+        <div class="bezel-brand">PIXELTRON</div>
+        <div class="crt">
+            <div class="screen">
+                <div class="screen-content">
